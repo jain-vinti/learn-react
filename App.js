c0b50d035a3fc96e1265a31f2ed217d3fefe3bb5
Vinti Jain <vintijain1998@gmail.com>
2023-10-11 16:49:21 -0700

    day 3: learn react JSX

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-); // This returns a React JS object with type h1
-
+// const heading = React.createElement("h1", { id: "heading" }, "This is Vinti");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); // This is used to convert the react object into html tag and add it to the DOM
+// React element is created by Babel while compiling this JSX code
+const titleElement = (
+  <h1 id="heading" className="heading">
+    This is Vinti from JSX element
+  </h1>
+);
+
+const TitleComponent = () => (
+  <h1 id="heading" className="heading">
+    This is Vinti from JSX component
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    <TitleComponent />
+    {console.log(titleElement)}
+    {titleElement}
+    <h1 className="heading">Vinti Jain demo of Functional Component</h1>
+  </div>
+);
+
+root.render(<HeadingComponent />);
